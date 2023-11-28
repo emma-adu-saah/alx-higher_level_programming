57ee01ceda801948a1c069bf95602c2db2ef0e33
emma-adu-saah <aduemma07@gmail.com>
2023-11-28 19:49:56 +0000

    Add 9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        pos_num = number * -1
+        last_digit = number % 10
+        last_digit = last_digit * -1
+    else:
+        last_digit = number % 10
+    print(f"{last_digit}", end="")
+    return last_digit
