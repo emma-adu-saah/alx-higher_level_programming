d9ad5d40ca53cd074218a787c1d347aaf9caa593
emma-adu-saah <aduemma07@gmail.com>
2023-11-28 19:59:12 +0000

    Corrected

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,8 @@
 def print_last_digit(number):
     if number < 0:
         pos_num = number * -1
-        last_digit = number % 10
+        print(f"Positive number: {pos_num}")
+        last_digit = pos_num % 10
         last_digit = last_digit * -1
     else:
         last_digit = number % 10
