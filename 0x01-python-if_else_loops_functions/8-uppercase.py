0ff89e2bdc4d9debea29b7783699bb191bb0b23d
emma-adu-saah <aduemma07@gmail.com>
2023-11-30 05:33:23 +0000

    Add correct

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,10 +6,6 @@ def uppercase(str):
         if string_ascii >= 97 and string_ascii <= 122:
             letter = string_ascii - 32
             final_str = final_str + chr(letter)
-            # print("{}".format(chr(letter)), end="")
         else:
-            # print("{}".format(chr(string_ascii)), end="")
             final_str = final_str + chr(string_ascii)
     print("{}".format(final_str))
-
-uppercase("IDHssueKjhdkeKJkJjfkHkehKdk")
