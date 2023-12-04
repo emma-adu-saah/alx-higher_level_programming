531b4cb1bcc08db11680c29f0a0216d3e7454cea
emma-adu-saah <aduemma07@gmail.com>
2023-12-04 20:07:51 +0000

    Correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argv = sys.argv
     argv_len = len(argv)
 
-    if argv_len >= 2:
+    if argv_len > 2:
         print("{} arguments:".format(argv_len))
         for i in range(1, argv_len):
             print("{}: {}".format(i, argv[i]))
