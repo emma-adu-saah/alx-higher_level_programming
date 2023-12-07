63f949876ac3dfc39ae977adb951fc196bb3699c
emma-adu-saah <aduemma07@gmail.com>
2023-12-07 11:17:08 +0000

    Correction

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    list_len = len(my_list) - 1
+    for i in range(0, list_len + 1):
+        print("{:d}".format(my_list[list_len - i]))
