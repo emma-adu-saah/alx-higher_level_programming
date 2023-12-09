ac72520082407ffa0a4ef013dbc465acdf01fdb0
emma-adu-saah <aduemma07@gmail.com>
2023-12-09 18:23:57 +0000

    Add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    best_score = 0
+    for i in a_dictionary:
+        if a_dictionary[i] > best_score:
+            best_score = a_dictionary[i]
+            name = i
+    return name
