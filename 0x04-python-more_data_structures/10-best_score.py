949bb85c129696f611c64d2a4ecca4b502862647
emma-adu-saah <aduemma07@gmail.com>
2024-01-24 18:05:02 +0000

    Correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     best_score = 0
     for i in a_dictionary:
         if a_dictionary[i] > best_score:
             best_score = a_dictionary[i]
-            name = i
+        name = i
     return name
