3c017ecfc7d344bc9cb9980ade63e0646ca61bb7
emma-adu-saah <aduemma07@gmail.com>
2024-02-27 04:08:06 +0000

    Update 'update()' method in rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,18 +101,31 @@ class Rectangle(Base):
                 print('#', end="")
             print('\n', end="")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assigns an argument to each attribute"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "id":
+                    self.id = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     @staticmethod
     def attribute_validator(attr_name, value):
