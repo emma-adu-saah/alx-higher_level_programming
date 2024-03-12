e60ce02cc1ee1f1fba877b8dbf02cf87d15da32b
emma-adu-saah <aduemma07@gmail.com>
2024-03-12 23:06:06 +0000

    Add 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,6 @@
 const Square1 = require('./5-square');
 
 class Square extends Square1 {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     if (c === undefined || c === null) { c = 'X'; }
     const character = c.repeat(this.size);
