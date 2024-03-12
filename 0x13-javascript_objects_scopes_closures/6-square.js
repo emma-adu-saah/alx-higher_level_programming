a294a35a5c4e8e0e047847dd213571a3500b37dd
emma-adu-saah <aduemma07@gmail.com>
2024-03-12 23:01:32 +0000

    Correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-const Square_1 = require('./5-square');
+const Square1 = require('./5-square');
 
-class Square extends Square_1 {
+class Square extends Square1 {
   constructor (size) {
     super(size);
     this.size = size;
   }
 
   charPrint (c) {
-    if (c === undefined) { c = 'X'; }
+    if (c === undefined || c === null) { c = 'X'; }
     const character = c.repeat(this.size);
     let i = 0;
     while (i < this.size) {
-	  console.log(character);
-	  i++;
+      console.log(character);
+      i++;
     }
   }
 }
