dd00956ea685c4159c04d2f063d952e35a493c4c
emma-adu-saah <aduemma07@gmail.com>
2024-03-13 13:20:05 +0000

    Correct 11-second_largest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  let newArray = args.sort();
-  newArray = newArray.reverse()[1];
-  console.log(newArray);
+  let newArray = args.slice(2);
+  newArray = newArray.sort((a, b) => b - a);
+  console.log(newArray[1]);
 }
