1260cdb27e4e7280e4b78eb49bce69086c5fedb8
emma-adu-saah <aduemma07@gmail.com>
2024-03-13 15:08:12 +0000

    Changes

--- 0x13-javascript_objects_scopes__closures/README.md
+++
@@ -1,3 +0,0 @@
-# [0x13. JavaScript - Objects, Scopes and Closures](0x13-javascript_objects_scopes_and_closures)
-
-[![js-semistandard-style](https://raw.githubusercontent.com/standard/semistandard/master/badge.svg)](https://github.com/standard/semistandard)
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,14 +2,16 @@
 const baseSquare = require('./5-square');
 
 class Square extends baseSquare {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
 
   charPrint (c) {
     if (c === undefined || c === null) { c = 'X'; }
     const character = c.repeat(this.size);
-    let i = 0;
-    while (i < this.size) {
+    for (let i = 0; i < this.size; i++) {
       console.log(character);
-      i++;
     }
   }
 }
