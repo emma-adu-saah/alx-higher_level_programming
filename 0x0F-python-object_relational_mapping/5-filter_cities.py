fa1f8f44c720e885beeac9d4ae0e540ea24e0b0b
emma-adu-saah <aduemma07@gmail.com>
2024-03-18 02:48:51 +0000

    Add task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all cities from the database hbtn_0e_4_usa"""
+import sys
+import MySQLdb
+
+ARGS = sys.argv[1:]
+
+
+def list_all_cities(username, password, db_name, state):
+    """List all cities from the database hbtn_0e_4_usa"""
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name\
+                    FROM cities LEFT JOIN states\
+                    ON states.id = cities.state_id\
+                    WHERE states.name = %s\
+                    ORDER BY cities.id", (state,))
+    result = cursor.fetchall()
+    print(", ".join([row for row in result]))
+
+    db.close()
+
+
+if __name__ == "__main__":
+    username = ARGS[0]
+    password = ARGS[1]
+    db_name = ARGS[2]
+    state = ARGS[3]
+
+    list_all_cities(username, password, db_name, state)
