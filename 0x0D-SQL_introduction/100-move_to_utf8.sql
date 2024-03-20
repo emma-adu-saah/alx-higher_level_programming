ca890c109e5e1f2d9d92ec277294abc4c87396d5
emma-adu-saah <aduemma07@gmail.com>
2024-03-20 01:27:35 +0000

    Changes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,5 @@
 -- Converts field name in first_table to UTF8
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 ALTER TABLE first_table MODIFY name VARCHAR(100) CHARACTER SET utf8mb4 
